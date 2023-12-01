d430088e9a915408bc995d6abaab844d2093a323
Rupesh Kumar Jena <rupesh.jena100@gmail.com>
2023-12-01 11:53:22 +0530

    New Videocall Module Added

--- analytics/src/main/java/com/techrev/analytics/Analytics.kt
+++ analytics/src/main/java/com/techrev/analytics/Analytics.kt
@@ -1,9 +1,11 @@
 package com.techrev.analytics
 
+import java.sql.DriverManager.println
+
 class Analytics () {
 
     companion object {
-        fun authenticateUser(mAuthKey : String) : String{
+        fun authenticateUser(mAuthKey : String) : String {
             println("User auth key: $mAuthKey");
             return "Your auth key $mAuthKey is authenticated successfully!"
         }
